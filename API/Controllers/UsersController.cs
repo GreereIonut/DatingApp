a5ceb3ee9e6515c168b7e9a335becccf648ae194
GreereIonut <65650700+GreereIonut@users.noreply.github.com>
2023-06-13 22:42:58 +0300

    angular

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -16,6 +16,7 @@ namespace API.Controllers
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
+
             _context = context;
         }
 
@@ -30,5 +31,6 @@ namespace API.Controllers
         {
             return await _context.Users.FindAsync(id);
         }
+        
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options=>options.UseSqlite(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
