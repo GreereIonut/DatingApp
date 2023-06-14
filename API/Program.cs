80561a40f2a03109e9d4cf1d83b3c59ce123d3ed
GreereIonut <65650700+GreereIonut@users.noreply.github.com>
2023-06-14 17:46:57 +0300

    part3

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
